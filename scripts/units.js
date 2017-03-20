0a536a00cc49219de22a2604ab22a2cd2f3fffcf
David Do <qldo1@student.monash.edu>
2017-03-20 22:17:11 +1100

    newly added

---
+++ scripts/units.js
@@ -0,0 +1,23 @@
+var units = [
+{
+	code:'FIT1001',
+	title:'Programming 1',
+	mark:'2014-10-14'
+},
+{
+	code:'FIT1002',
+	title:'Programming 2',
+	mark:'2014-10-14'
+},
+{
+	code:'FIT1003',
+	title:'Other Irrelevant Topics',
+	mark:'2014-10-14'
+	
+},
+{
+	code:'FIT1004',
+	title:'More Irrelevant Topics',
+	mark:'2014-10-14'
+}
+];
---
+++ taskUnits.js
@@ -0,0 +1,23 @@
+var taskUnits = [
+{
+	name:'Return Library book',
+	date:'20-11-2015',
+	type:'work'
+},
+{
+	name:'Play soccer',
+	date:'12-21-2001',
+	type:'work'
+},
+{
+	name:'practice IT',
+	date:'11-22-2016',
+	type:'work'
+	
+},
+{
+	name:'GYM',
+	date:'12-9-2016',
+	type:'Personal'
+}
+];
